a708f070802f0b8d9ab0ed8d811e9e2277c4c5b3
pltrdy <pltrdy@gmail.com>
2016-12-19 12:38:13 +0100

    wip scripting

--- tools/get_ptb.sh
+++ tools/get_ptb.sh
@@ -3,12 +3,17 @@
 echo "Downloading PTB Dataset... (http://www.fit.vutbr.cz/~imikolov/rnnlm/simple-examples.tgz)"
 wget http://www.fit.vutbr.cz/~imikolov/rnnlm/simple-examples.tgz
 
-echo "Extracting..."
-tar -xzf simple-examples.tgz
+# wget may produce simple-examples.tgz.1 if the file already exists
+# we extract the last one
+file=$(ls -srl | awk '{print $NF}' | grep simple-examples.tgz | head -1)
+
+echo "Extracting $file"
+tar -xzf "$file"
 
 # Creating symlinks because PTB use 'ptb.' prefixed files. Our script wants files without this prefix
 cd simple-examples/data
 for f in ptb.*; do ln -s "$f"  "$(echo "$f" | cut -c 5-)"; done;
 cd ../..
 
+echo "Creating symlink ./data -> simple-examples/data"
 ln -s simple-examples/data data
